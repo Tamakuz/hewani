4b12a7015dd86ea23ed0adfedb37854488022161
mbahhadi <hadzipamuji5@gmail.com>
2024-11-07 17:08:53 +0700

    admin-fix

--- app/(server)/config/imgkit.ts
+++ app/(server)/config/imgkit.ts
@@ -1,9 +1,10 @@
 import ImageKit from "imagekit";
 
+// Buat instance ImageKit dengan kredensial yang sudah ada
 const imagekit = new ImageKit({
-  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
-  privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY as string,
-  urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT as string,
+  publicKey: "public_AehC/obgZBbd2iGqKO7/9pc3TXY=",
+  privateKey: "private_EVMmtwvqAKA6/K0EVuqG/bEhBpQ=",
+  urlEndpoint: "https://ik.imagekit.io/tamakuz",
 });
 
 export default imagekit;
