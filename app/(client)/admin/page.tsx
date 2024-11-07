f51e1ec93d52c94dcacf95d4c7db44edad444148
HADZI PAMUJI <50190490+HadZzz@users.noreply.github.com>
2024-11-07 14:26:24 +0700

    Update page.tsx

--- app/(client)/admin/page.tsx
+++ app/(client)/admin/page.tsx
@@ -228,7 +228,7 @@ const ModernDashboard = () => {
               <div className="space-y-4">
                 <div className="flex justify-between items-center flex-wrap gap-4">
                   <div className="flex items-center gap-4">
-                    <AddProductForm />
+                   <button>tambah product</button>
                     <div className="relative">
                       <Search className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" />
                       <Input
